3b20ba89112707c4b2e21d964ea8e3f1af41816e
orangen-2k <orangen2000@gmail.com>
2020-03-25 10:45:23 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ export class AppComponent implements OnInit  {
    ngOnInit() {
   }
   databv: Array<Benhvien> = [
-    new Benhvien(1, "FPT Polytechnic", 'https://i.ytimg.com/vi/38SIAv0q5jA/hqdefault.jpg',"Vũ Chí Thành","Hà Nội",100,100,0),
-    new Benhvien(2, "Bách khoa", 'https://i.ytimg.com/vi/uu31fKVmWw0/maxresdefault.jpg',"Lê Lộc Cao","Đà Nẵng",17,3,2),
-    new Benhvien(3, "FPT Univorcity",'https://i.ytimg.com/vi/tQV3MrnZUyg/maxresdefault.jpg', "Phùng Chí Thanh",3,14,7),
-    new Benhvien(4, "Sư phạm",'https://i.ytimg.com/vi/ohw1P8RbsIs/maxresdefault.jpg',"Phạm Ngọc Thạo","Cà Mau",14,2,6),
+    new Benhvien(1, "FPT Polytechnic", 'https://photo-2-baomoi.zadn.vn/w1000_r1/2019_10_09_146_32494582/0ad50da5bde554bb0df4.jpg',"Vũ Chí Thành","Hà Nội",100,100,0),
+    new Benhvien(2, "Bách khoa", 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLquwL3U9c4MwkN0TKUo4UUHKcrZU8PeYQMVRmWEe3Ru4ppH2o',"Lê Lộc Cao","Đà Nẵng",17,3,2),
+    new Benhvien(3, "FPT Univorcity",'https://www.vietinbank.vn/sites/mediafile/CDL066787', "Phùng Chí Thanh",3,14,7),
+    new Benhvien(4, "Sư phạm",'https://photo-1-baomoi.zadn.vn/w1000_r1/2019_11_21_188_33042825/6eb482440a04e35aba15.jpg',"Phạm Ngọc Thạo","Cà Mau",14,2,6),
   ];
 
 
